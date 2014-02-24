7d453e8ba908e9cd9263f75832f7c4eba17dc6f9
think.l.h <think.l.h@gmail.com>
2014-02-24 20:34:38 +0800

    Rule文件无法被正确加载的异常

---
+++ Bools/bools-core/src/main/java/org/daisy/bools/core/build/RuleFileException.java
@@ -0,0 +1,9 @@
+package org.daisy.bools.core.build;
+
+public class RuleFileException extends Exception {
+	
+	public RuleFileException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
